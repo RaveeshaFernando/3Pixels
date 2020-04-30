26b0594980c5d2ebffc06fa38f62300bf6c6bebb
Raveesha Fernando <rdilshanf@gmail.com>
2020-04-30 14:22:59 +0530

    frontend deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "...",
+    authDomain: "pixels-db453.firebaseapp.com",
+    databaseURL: "https://pixels-db453.firebaseio.com",
+    projectId: "pixels-db453",
+    storageBucket: "pixels-db453.appspot.com",
+    messagingSenderId: "903110716844",
+  }
 };
